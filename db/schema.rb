9b8c40956ce26ecaefda4f090f434ab1a2c758bb
jakeatwork <jakedlevine2@gmail.com>
2014-04-24 11:34:30 -0700

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424180902) do
+ActiveRecord::Schema.define(version: 20140424183008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.datetime "date"
+    t.integer  "venue_id"
+    t.integer  "performer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "guests", force: true do |t|
+    t.string   "event_id"
+    t.integer  "type_id"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "lists", force: true do |t|
+    t.integer  "venue_id"
+    t.integer  "event_id"
+    t.integer  "performer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "performers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "types", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -35,5 +72,11 @@ ActiveRecord::Schema.define(version: 20140424180902) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "venues", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
