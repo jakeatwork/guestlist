c32b16a725455afcac918614a6bc60c184ecb807
jakeatwork <jakedlevine2@gmail.com>
2014-04-24 16:16:52 -0700

    Annotate

--- app/models/guests_list.rb
+++ app/models/guests_list.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: guests_lists
+#
+#  id         :integer          not null, primary key
+#  guest_id   :integer
+#  list_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class GuestsList < ActiveRecord::Base
 end
--- spec/models/guests_list_spec.rb
+++ spec/models/guests_list_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: guests_lists
+#
+#  id         :integer          not null, primary key
+#  guest_id   :integer
+#  list_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe GuestsList do
