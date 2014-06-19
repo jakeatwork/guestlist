5e528a77a11be14721c2f36b18f0a254d5629706
jakeatwork <jakedlevine2@gmail.com>
2014-06-19 14:49:21 -0700

    Save user name and email from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,8 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.email = auth.info.email
+      user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Guestlist::Application.routes.draw do
   resources :events
 
   root :to => "home#index"
-  # devise_for :users, :controllers => {:registrations => "registrations"}
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {omniauth_callbacks: "users/omniauth_callbacks", :registrations => 'registrations' }
 
   resources :users
