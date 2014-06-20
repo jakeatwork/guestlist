3dd3700865fd88200dc457f1a5d5404ee6fdd877
jakeatwork <jakedlevine2@gmail.com>
2014-06-20 02:46:43 -0700

    Add user filter by provider is facebook on user list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @users = User.all
+    # @users = User.all
+    @users = User.where(:provider => "facebook")
   end
 
   def show
