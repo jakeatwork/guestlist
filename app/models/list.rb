12244e37783faed4f024ab067628899a35d0f684
jakeatwork <jakedlevine2@gmail.com>
2014-06-23 16:10:35 -0700

    Last bit of association cleanup...I hope...

--- app/models/list.rb
+++ app/models/list.rb
@@ -14,6 +14,7 @@
 
 class List < ActiveRecord::Base
 	belongs_to :event
-	has_many :users
+	belongs_to :user
+	has_many :types
 	resourcify
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -9,5 +9,5 @@
 #
 
 class Type < ActiveRecord::Base
-	belongs_to :guest
+	belongs_to :list
 end
